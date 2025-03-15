47d41084bae5889a8ae3bb9234baf81733f0c737
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-03-15 23:24:18 +0300

    created login and register logic for end points

--- config/model.js
+++ config/model.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     required: true,
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
+import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./config/dbconnect.js";
 import User from "./config/model.js";
@@ -11,29 +12,66 @@ const app = express();
 const PORT = 4000;
 
 // server middleware
+
+// CORS Configuration
+const allowedOrigins = ["http://localhost:5173"];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // user authentication api end points
 
 // POST - create user
 app.post("/api/auth/user-register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json({ message: "User already exisits. Please sign in" });
+  }
+
   try {
-    const { email, password } = req.body;
     const salt = bcrypt.genSaltSync(10);
     const hashPassword = bcrypt.hashSync(password, salt);
-    const user = await User.create({ email, password: hashPassword });
+    const user = await User.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+    });
 
-    res.json({ user: user });
-  } catch (error) {
-    console.log(error.message);
+    return res.status(201).json("");
+  } catch (err) {
+    return res.status(400).json({ message: "User already exisits. Please sign in" });
   }
 });
 
 // POST - login user
 app.post("/api/auth/user-login", async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
-  res.send(`User logged in`);
+
+  try {
+    const user = await User.findOne({ email }).exec();
+
+    if (!user | !bcrypt.compare(password, user.password)) {
+      return res.status(400).json({ message: "Not authorized" });
+    } else {
+      return res.json({ name: user.name });
+    }
+  } catch (err) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
 });
 
 app.listen(PORT, () => {
